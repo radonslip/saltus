925ff9878f4494b82fcfba6e1a5173adefb4af16
Angerydoge <jarvisjameshutchinson@gmail.com>
2022-07-02 18:29:31 +0100

    playerMovmentUpdate

--- Assets/scripts/move.cs
+++ Assets/scripts/move.cs
@@ -8,25 +8,51 @@ public class move : MonoBehaviour
     public int moveSpeed;
     public int jumpForce;
     private Rigidbody2D body;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
+        if(Input.GetKeyDown("space") && onGround())
         {
             body.velocity = new Vector2(0,jumpForce);
         }
 
+
+    }
+
+    void FixedUpdate()
+    {
+
+
         float vecX = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(vecX * moveSpeed, body.velocity.y);
 
+        if(vecX > 0)
+        {
+            gameObject.transform.localScale = new Vector3(10,10,1);
+        }
+
+        else if(vecX < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-10,10,1);
+        }
 
 
+
+    }
+
+    private bool onGround()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
