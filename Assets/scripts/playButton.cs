84e4f94800242c7407ac78a22e361e6d11170a09
Angerydoge <jarvisjameshutchinson@gmail.com>
2022-07-06 10:36:57 +0100

    bussy

--- Assets/scripts/playButton.cs
+++ Assets/scripts/playButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playButton : gameManager
+public class playButton : MonoBehaviour
 {
     //Create scripts by right clicking in unity it does stuff above for you
 }
