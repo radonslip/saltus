a365f7f24fe4511940bc1d398b8b7c95734113a8
genesisC00kie <dylancook190904@gmail.com>
2022-07-06 14:55:30 +0100

    button progress

--- Assets/scripts/playButton.cs
+++ Assets/scripts/playButton.cs
@@ -2,9 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playButton : MonoBehaviour
 {
-    //Create scripts by right clicking in unity it does stuff above for you
+    public Button PB;
+    public void Start () {
+        Button btn = PB.GetComponent<Button>();
+        btn.onClick.AddListener(onPress);
+    }
+    void onPress(){
+        SceneManager.LoadScene(sceneName:"testMapWet");
+        
+    }
 }
 
