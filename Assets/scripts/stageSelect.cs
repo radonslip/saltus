d9e6593a0ac640de3ed5fc502560324d5bfc4d7f
genesisC00kie <dylancook190904@gmail.com>
2022-07-14 22:58:04 +0100

    stage select DONE!!!
    all thats needed is more stages to switch between

---
+++ Assets/scripts/stageSelect.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class stageSelect : MonoBehaviour
+{
+    public Button SSB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = SSB.GetComponent<Button>();
+        Debug.Log(worldSelectButton.loadedWorld);
+        btn.onClick.AddListener(onPress);
+        
+    }
+
+    // Update is called once per frame
+   public void onPress()
+    {
+        var ButtonText = SSB.GetComponentInChildren<Text>();
+        Debug.Log(ButtonText.text);
+
+        
+        if(worldSelectButton.loadedWorld == "1")
+        {
+            Debug.Log("world 1");
+            if(ButtonText.text == "stage1")
+            {
+                Debug.Log("1-1");
+                SceneManager.LoadScene(sceneName:"beachLevel1");
+            }
+            else if(ButtonText.text == "stage2")
+            {
+                Debug.Log("1-2");
+                SceneManager.LoadScene(sceneName:"beachLevel2");
+            }
+            else if(ButtonText.text == "stage3")
+            {
+                Debug.Log("1-3");
+                SceneManager.LoadScene(sceneName:"beachLevel3");
+            }
+        }
+        else if(worldSelectButton.loadedWorld == "2")
+        {
+            if(ButtonText.text == "stage1")
+            {
+                Debug.Log("2-1");
+                SceneManager.LoadScene(sceneName:"forestLevel1");
+            }
+            else if(ButtonText.text == "stage2")
+            {
+                Debug.Log("2-2");
+                SceneManager.LoadScene(sceneName:"forestLevel2");
+            }
+            else if(ButtonText.text == "stage3")
+            {
+                Debug.Log("2-3");
+                SceneManager.LoadScene(sceneName:"forestLevel3");
+            }
+        }
+        else if(worldSelectButton.loadedWorld == "3")
+        {
+            if(ButtonText.text == "stage1")
+            {
+                Debug.Log("3-1");
+                SceneManager.LoadScene(sceneName:"mountainLevel1");
+            }
+            else if(ButtonText.text == "stage2")
+            {
+                Debug.Log("3-2");
+                SceneManager.LoadScene(sceneName:"mountainLevel2");
+            }
+            else if(ButtonText.text == "stage3")
+            {
+                Debug.Log("3-3");
+                SceneManager.LoadScene(sceneName:"mountainLevel3");
+            }
+        else if(worldSelectButton.loadedWorld == "4")
+        {
+            if(ButtonText.text == "stage1")
+            {
+                Debug.Log("4-1");
+                SceneManager.LoadScene(sceneName:"templeLevel1");
+            }
+            else if(ButtonText.text == "stage2")
+            {
+                Debug.Log("4-2");
+                SceneManager.LoadScene(sceneName:"templeLevel2");
+            }
+            else if(ButtonText.text == "stage3")
+            {
+                Debug.Log("4-3");
+                SceneManager.LoadScene(sceneName:"templeLevel3");
+            }
+        }
+        }
+}}
---
+++ Assets/scripts/worldSelectButton.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class worldSelectButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static string loadedWorld = "0";
+    public Button WSB;
+    public void Start()
+    {
+        Button btn = WSB.GetComponent<Button>();
+        
+        btn.onClick.AddListener(onPress);
+        
+    }
+
+    // Update is called once per frame
+    public void onPress()
+    {
+        var ButtonText = WSB.GetComponentInChildren<Text>();
+        Debug.Log(ButtonText.text);
+
+        
+        if(ButtonText.text == "ForestButton")
+        {
+            loadedWorld = "2";
+                SceneManager.LoadScene(sceneName:"StageSelect");
+        }
+        else if (ButtonText.text == "beachButton")
+        {       
+                loadedWorld = "1";
+                SceneManager.LoadScene(sceneName:"StageSelect");
+        }
+        else if (ButtonText.text == "MountainButton")
+        {
+            loadedWorld = "3";
+                SceneManager.LoadScene(sceneName:"StageSelect");
+        }
+        else if (ButtonText.text == "TempleButton")
+        {
+            loadedWorld = "4";
+                SceneManager.LoadScene(sceneName:"StageSelect");
+        }
+        }
+
+    }
+
